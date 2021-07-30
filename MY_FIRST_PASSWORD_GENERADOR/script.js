dddb0fcde00bd08da5c3089847ab1b040c652211
Michael De Lagrave Viau <86207868+MichaelDViau@users.noreply.github.com>
2021-07-30 12:53:09 -0700

    Update script.js

--- MY_FIRST_PASSWORD_GENERADOR/script.js
+++ MY_FIRST_PASSWORD_GENERADOR/script.js
@@ -1,46 +1,72 @@
-
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password inputfunction generatePass(pLength){
+// Item to use for my password such as case,numbers, characters - linked to the fonction and/array 
 
-  function generateBtn()
-  {
+function generatePassword() {
+
+var uppercaseArray = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var lowercaseArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o",  "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var numbersArray =      ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var characterArray = ["&", "*", "(", ")", "-", "_","@", "#", "$", "%", "^", "=", "+"];
+
+// Results and user Array 
+ 
+var resultArray = [];
+var userArray = [];
+
+// Question that we want to ask to the user liked with the diff. var previously put 
+uppercaseArray [1] 
+
+  var numCharacter = prompt ("How any itme do you want to use for your password - between 8 and 128 ?");
+  var numbers = confirm     ("Do you want to use numbers in your password?");
+  var uppercases = confirm  ("Do you want to use Uppercases in your password?");
+  var lowercases = confirm  ("Do you want to use lowercases in your password?");
+  var characters = confirm  ("Do you want to use special characters in your password?");
+
+
+
+// Statement saying true of false to generate ramdoness 
+if (numbers){
+  resultArray = resultArray.concat(numbersArray);
   
-  
-  var uppercase = document.getElementById("uppercase").checked === true ? true : false;
-  var lowercase = document.getElementById("lowercase").checked === true ? true : false;
-  var numbers = document.getElementById("numbers").checked === true ? true : false;
-  var symbols = document.getElementById("symbols").checked === true ? true : false;
-  var len = document.getElementById("len").value;
-  
-  
-  var str= [ ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"],
-  ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"],
-  ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"],
-  ["!", "#", "$", "&", "%", "'", "(", ")", "*", "+", "-", "/", ":", ";", "<", "=", ">", "?", "@", "^", "_", "~", "`", "{", "|", "}", "."]];
-  var str_user;
-  uppercase===true ? str_user=str[0] :0;
-  lowercase===true ? str_user+=str[1] :0;
-  numbers===true ? str_user+=str[2] :0;
-  symbols===true ? str_user+=str[3] :0;
-  if(str_user){
-    var v= Math.floor(Math.random() * str_user.length);
-    var password = str_user[v];
-  
-    for (let i = 0 ; i<len ; i++ )
-    { 
-      v= Math.floor(Math.random() * str_user.length);
-      console.log(v);
-      password += str_user[v];
-    }
-    console.log(password);
-    document.getElementById("password").value =password;
-  }else{
-   
-    alert('Minmum check mark 1 option for generation password');
-  
-  }
+}
+
+if (uppercases){
+  resultArray = resultArray.concat(uppercaseArray);
+
+}
+
+if (lowercases){
+  resultArray = resultArray.concat(lowercaseArray);
+
+}
+
+if (characters){
+  resultArray = resultArray.concat(characterArray);
+}
+console.log(resultArray)
+
+
+for (var i = 0; i < numCharacter; i++) {
+      
+  userArray.push (resultArray[Math.floor(Math.random() * resultArray.length)]); 
   }
 
-console.log( generateBtn() );
+  return userArray.join("") ;
+}
+
+// The results we are expecting the JS to complete over the user interface = Provde a random password 
+
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+
+}
+
+
+// An Event listener to generate the click results - meaning that this will allow the ''mouse click'' for the js code to operate 
 generateBtn.addEventListener("click", writePassword);
+
